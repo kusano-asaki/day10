77833388e3eefee2ac2056196db1b04c6a039827
yasuyasu-ot <y.otsubo.dss@gmail.com>
2020-06-06 18:00:36 +0900

    #2 セッションログイン・ログアウト画面の実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,22 @@
 # frozen_string_literal: true
 
 class SessionsController < ApplicationController
+  def new; end
+
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user&.authenticate(params[:session][:password])
-      # ログイン成功した場合
+      session[:user_id] = user.id
+      redirect_to user_path(user.id)
     else
-      flash.now[:danger] = 'ログインに失敗しました'
+      flash[:danger] = 'ログインに失敗しました'
       render :new
     end
+  end
+
+  def destroy
+    session.delete(:user_id)
+    flash[:notice] = 'ログアウトしました'
+    redirect_to new_session_path
   end
 end
